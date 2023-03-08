4ddeca96e0b44033329ec492ec18fd57d72eb54b
Alex <alex.sircu@gmail.com>
2023-03-08 10:10:02 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 500;
+short b = 100;
+sbyte c = -50;
+long d = 1_000_000;
+int e = 0b10101010;
+ushort f = 0x0A56;
+Console.WriteLine("{0} {1} {2} {3} {4} {5}", a, b, c, d, e, f);
+Console.WriteLine($"{a} {b} {c} {d} {e} {f}");
